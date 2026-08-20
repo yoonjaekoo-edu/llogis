ae9e691bdb26385266f3a28af78c3b0300d6b9a9
구윤재 <bits.stdcpp.h1207@gmail.com>
2026-08-20 23:16:47 +0900

    feat: replace Doge market mount with stock investing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
-import DogeMarketNav from './DogeMarketNav'
+import StockMarketNav from './StockMarketNav'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider context={{}}>
       <App />
-      <DogeMarketNav />
+      <StockMarketNav />
     </HelmetProvider>
   </React.StrictMode>,
 )
