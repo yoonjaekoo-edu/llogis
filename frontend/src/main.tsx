e4f559b783a2cf26c23948af5250167dcbd65822
구윤재 <cstdio.code@gmail.com>
2026-09-02 18:28:39 +0900

    DiceBear 기본 아바타 적용

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
+import { installDiceBearAvatarDefaults } from './dicebearAvatar'
+
+installDiceBearAvatarDefaults()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
